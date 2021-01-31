3b67fd9c6b8bbffe63f81e30164fbecefd4a3071
VladSobko <vldsbk11@gmail.com>
2021-01-31 14:56:51 +0200

    conflict

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>QWERTYnfewfnewj</p>
         <p>CHANGEasdas</p>
         <p>CHANGE1</p>
+        <p>122345</p>
         <p>CHANGE2</p>
         <p>CHANGE3</p>
         <p>CHANGE from MAC</p>
